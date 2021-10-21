936997e4b553014edf91fe3291d94bff261ccb54
Pleket <50240262+Pleket@users.noreply.github.com>
2021-10-21 15:02:40 +0200

    Quick edit for merge

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,6 +18,7 @@ export default defineComponent({
       {label: 'Phonemes', icon: 'pi pi-fw pi-comment', to: '/phonemes'},
       {label: 'Words', icon: 'pi pi-fw pi-comments', to: '/words'},
       {label: 'Audio', icon: 'pi pi-fw pi-play', to: '/audio'},
+      {label: 'Sign In', icon: 'pi pi-fw pi-sign-in', to: '/SignIn'}
       // {label: 'Theme', icon: 'pi pi-fw pi-palette', command:() => {themeChanger.darkThemeSwitch()}}
     ];
 
