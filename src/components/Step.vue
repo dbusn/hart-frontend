4dcedd3664e175605254f4118512eb2715630784
Tessgeerts <83406587+Tessgeerts@users.noreply.github.com>
2021-12-24 11:43:29 +0100

    commented until the working stage without working functions
    in the import, apiwrapper, getrandom and the createapp need to be uncommented when letting the functions work

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -55,176 +55,179 @@
 import {defineComponent} from "vue";
 import Button from "primevue/button";
 import Panel from "primevue/panel";
+import Checkbox from "primevue/checkbox";
+import Dropdown from "primevue/dropdown";
+import Fieldset from "primevue/fieldset";
 // import {getRandom} from "@/helpers/array.helper";
 
 export default defineComponent({
   name: 'Step',
   props: [ "selectedPhonemes", "StepNumber" ],
   components: {Panel},
-  extends: {Button},
-
+  extends: {Button, Checkbox, Dropdown, Fieldset},
+  //
   // setup: async () => {
-
-    // // Get phoneme data from the backend
-    // const phonemeData = (await APIWrapper.getPhonemes()).phonemes;
-    // // const phonemeData = ["AH", "K"];
-    //
-    // // Initialize variables used throughout component
-    // const selectedTrainPhonemes = ref([]);
-    // const dropdownPhoneme = ref();
-    // const fiRows = ref(0);
-    // const raRows = ref(0);
-    // const identificationActive = ref(false);
-    //
-    // const playedPhoneme = ref();
-    // /**
-    //  * Function for sending a phoneme from the dropdown menu.
-    //  */
-    // function sendDropdownPhoneme() {
-    //   // Check if something was selected
-    //   if (dropdownPhoneme.value !== undefined) {
-    //     // Create json and send to backend
-    //     const json = {'phonemes': [dropdownPhoneme.value.name]}
-    //     APIWrapper.sendPhonemeMicrocontroller(json);
-    //   } else {
-    //     // If nothing selected, alert user
-    //     alert("Please select a phoneme to send");
-    //   }
-    // }
-    //
-    // /**
-    //  * Function for executing forced identification behavior.
-    //  */
-    // function sendForcedIdentification() {
-    //   // Get div from page for placing buttons
-    //   const buttonDiv = document.getElementById("forcedIdentificationButtons")
-    //   if (buttonDiv === null) {
-    //     return;
-    //   }
-    //
-    //   // empty button div
-    //   buttonDiv.innerHTML = '';
-    //
-    //   // check if some phonemes are selected
-    //   if (selectedTrainPhonemes.value.length === 0) {
-    //     alert("Please select phonemes to train on");
-    //     return;
-    //   }
-    //
-    //   // get a set of random phonemes from the selected phonemes
-    //   const randomPhonemes = (selectedTrainPhonemes.value as any)
-    //   playedPhoneme.value = getRandom(randomPhonemes, 1)[0];
-    //   identificationActive.value = true;
-    //
-    //   // Send selected phoneme to backend
-    //   APIWrapper.sendPhonemeMicrocontroller({'phonemes': [playedPhoneme.value]});
-    //
-    //   // Increase the number of forced identification rounds.
-    //   fiRows.value++;
-    //
-    //   // Get the answer table
-    //   const pTable = document.getElementById("phoneme-table");
-    //   if (pTable === null) {
-    //     return
-    //   }
-    //
-    //   // Create new row element for table
-    //   const row = document.createElement("tr");
-    //   row.innerHTML = "<td>" + fiRows.value + "</td><td>" + playedPhoneme.value + "</td><td id='pTableRow_" + fiRows.value + "'></td>";
-    //   pTable.appendChild(row);
-    //
-    //   // Add explanation div
-    //   const textDiv = document.createElement('div');
-    //   textDiv.innerHTML = '<p>Which phoneme was just played?</p>';
-    //   buttonDiv.appendChild(textDiv);
-    //
-    //   // For each of the phonemes selected, create buttons and assign listeners to it.
-    //   randomPhonemes.forEach((phoneme: string) => {
-    //     // Create div for button
-    //     const div = document.createElement('div');
-    //     div.style.display = "inline-block";
-    //     div.style.marginRight = "10px";
-    //
-    //     // Add div for button to the button div
-    //     buttonDiv.appendChild(div);
-    //     createApp(Button, {
-    //       label: phoneme,
-    //       id: "fid_" + phoneme,
-    //       class: "p-shadow-2",
-    //       style: "margin-bottom: 4px"
-    //     }).mount(div);
-    //
-    //     // Get the button from the page
-    //     const btn = document.getElementById("fid_" + phoneme);
-    //
-    //     // Get the table cell for guesses from the page
-    //     const guessesCell = document.getElementById("pTableRow_" + fiRows.value);
-    //     if (btn === null || guessesCell === null) {
-    //       return
-    //     }
-    //
-    //     // Add button event listener
-    //     btn.addEventListener("click", () => {
-    //       const bgColor = btn.style.background;
-    //       if (phoneme === playedPhoneme.value) {
-    //         btn.style.background = "green";
-    //         guessesCell.innerHTML += "<span style='margin-right: 4px; margin-bottom: 4px; padding: 5px; color: #8800FF; font-weight: bolder'>" + phoneme + "</span>";
-    //       } else {
-    //         btn.style.background = "red";
-    //         guessesCell.innerHTML += "<span style='margin-right: 4px; margin-bottom: 4px; padding: 5px'>" + phoneme + "</span>";
-    //       }
-    //       setTimeout(() => {
-    //         btn.style.background = bgColor
-    //       }, 1000);
-    //     });
-    //   })
-    // }
-    //
-    // function repeatPreviousPhoneme() {
-    //   APIWrapper.sendPhonemeMicrocontroller({'phonemes': [playedPhoneme.value]});
-    // }
-    //
-    // // Format gotten phonemes from backend.
-    // // const phonemes: { name: string }[] = [];
-    // // phonemeData.forEach((pho: string) => {
-    // //   phonemes.push({name: pho})
-    // // })
-    //
-    // const phonemes: { name: string }[] = [];
-    // phonemeData.forEach((pho: string) => {
-    //   phonemes.push({name: pho})
-    // })
-    //
-    // /**
-    //  * Function for selecting all phonemes
-    //  */
-    // function selectAllPhonemes() {
-    //   selectedTrainPhonemes.value = []
-    //   phonemeData.forEach((pho: string) => {
-    //     (selectedTrainPhonemes.value as string[]).push(pho);
-    //   })
-    // }
-    //
-    // function deselectAllPhonemes() {
-    //   selectedTrainPhonemes.value = [];
-    // }
-    //
-    // // Return all variables
-    //return {
-      // phonemes,
-      // selectedTrainPhonemes,
-      // dropdownPhoneme,
-      // fiRows,
-      // raRows,
-      // identificationActive,
-      //
-      // sendDropdownPhoneme,
-      // sendForcedIdentification,
-      // repeatPreviousPhoneme,
-      // selectAllPhonemes,
-      // deselectAllPhonemes
-    // }
-  //},
+  //
+  //   // Get phoneme data from the backend
+  //   const phonemeData = (await APIWrapper.getPhonemes()).phonemes;
+  //   // const phonemeData = ["AH", "K"];
+  //
+  //   // Initialize variables used throughout component
+  //   const selectedTrainPhonemes = ref([]);
+  //   const dropdownPhoneme = ref();
+  //   const fiRows = ref(0);
+  //   const raRows = ref(0);
+  //   const identificationActive = ref(false);
+  //
+  //   const playedPhoneme = ref();
+  //   /**
+  //    * Function for sending a phoneme from the dropdown menu.
+  //    */
+  //   function sendDropdownPhoneme() {
+  //     // Check if something was selected
+  //     if (dropdownPhoneme.value !== undefined) {
+  //       // Create json and send to backend
+  //       const json = {'phonemes': [dropdownPhoneme.value.name]}
+  //       APIWrapper.sendPhonemeMicrocontroller(json);
+  //     } else {
+  //       // If nothing selected, alert user
+  //       alert("Please select a phoneme to send");
+  //     }
+  //   }
+  //
+  //   /**
+  //    * Function for executing forced identification behavior.
+  //    */
+  //   function sendForcedIdentification() {
+  //     // Get div from page for placing buttons
+  //     const buttonDiv = document.getElementById("forcedIdentificationButtons")
+  //     if (buttonDiv === null) {
+  //       return;
+  //     }
+  //
+  //     // empty button div
+  //     buttonDiv.innerHTML = '';
+  //
+  //     // check if some phonemes are selected
+  //     if (selectedTrainPhonemes.value.length === 0) {
+  //       alert("Please select phonemes to train on");
+  //       return;
+  //     }
+  //
+  //     // get a set of random phonemes from the selected phonemes
+  //     const randomPhonemes = (selectedTrainPhonemes.value as any)
+  //     playedPhoneme.value = getRandom(randomPhonemes, 1)[0];
+  //     identificationActive.value = true;
+  //
+  //     // Send selected phoneme to backend
+  //     APIWrapper.sendPhonemeMicrocontroller({'phonemes': [playedPhoneme.value]});
+  //
+  //     // Increase the number of forced identification rounds.
+  //     fiRows.value++;
+  //
+  //     // Get the answer table
+  //     const pTable = document.getElementById("phoneme-table");
+  //     if (pTable === null) {
+  //       return
+  //     }
+  //
+  //     // Create new row element for table
+  //     const row = document.createElement("tr");
+  //     row.innerHTML = "<td>" + fiRows.value + "</td><td>" + playedPhoneme.value + "</td><td id='pTableRow_" + fiRows.value + "'></td>";
+  //     pTable.appendChild(row);
+  //
+  //     // Add explanation div
+  //     const textDiv = document.createElement('div');
+  //     textDiv.innerHTML = '<p>Which phoneme was just played?</p>';
+  //     buttonDiv.appendChild(textDiv);
+  //
+  //     // For each of the phonemes selected, create buttons and assign listeners to it.
+  //     randomPhonemes.forEach((phoneme: string) => {
+  //       // Create div for button
+  //       const div = document.createElement('div');
+  //       div.style.display = "inline-block";
+  //       div.style.marginRight = "10px";
+  //
+  //       // Add div for button to the button div
+  //       buttonDiv.appendChild(div);
+  //       createApp(Button, {
+  //         label: phoneme,
+  //         id: "fid_" + phoneme,
+  //         class: "p-shadow-2",
+  //         style: "margin-bottom: 4px"
+  //       }).mount(div);
+  //
+  //       // Get the button from the page
+  //       const btn = document.getElementById("fid_" + phoneme);
+  //
+  //       // Get the table cell for guesses from the page
+  //       const guessesCell = document.getElementById("pTableRow_" + fiRows.value);
+  //       if (btn === null || guessesCell === null) {
+  //         return
+  //       }
+  //
+  //       // Add button event listener
+  //       btn.addEventListener("click", () => {
+  //         const bgColor = btn.style.background;
+  //         if (phoneme === playedPhoneme.value) {
+  //           btn.style.background = "green";
+  //           guessesCell.innerHTML += "<span style='margin-right: 4px; margin-bottom: 4px; padding: 5px; color: #8800FF; font-weight: bolder'>" + phoneme + "</span>";
+  //         } else {
+  //           btn.style.background = "red";
+  //           guessesCell.innerHTML += "<span style='margin-right: 4px; margin-bottom: 4px; padding: 5px'>" + phoneme + "</span>";
+  //         }
+  //         setTimeout(() => {
+  //           btn.style.background = bgColor
+  //         }, 1000);
+  //       });
+  //     })
+  //   }
+  //
+  //   function repeatPreviousPhoneme() {
+  //     APIWrapper.sendPhonemeMicrocontroller({'phonemes': [playedPhoneme.value]});
+  //   }
+  //
+  //   // Format gotten phonemes from backend.
+  //   // const phonemes: { name: string }[] = [];
+  //   // phonemeData.forEach((pho: string) => {
+  //   //   phonemes.push({name: pho})
+  //   // })
+  //
+  //   const phonemes: { name: string }[] = [];
+  //   phonemeData.forEach((pho: string) => {
+  //     phonemes.push({name: pho})
+  //   })
+  //
+  //   /**
+  //    * Function for selecting all phonemes
+  //    */
+  //   function selectAllPhonemes() {
+  //     selectedTrainPhonemes.value = []
+  //     phonemeData.forEach((pho: string) => {
+  //       (selectedTrainPhonemes.value as string[]).push(pho);
+  //     })
+  //   }
+  //
+  //   function deselectAllPhonemes() {
+  //     selectedTrainPhonemes.value = [];
+  //   }
+  //
+  //   // Return all variables
+  //   return {
+  //     phonemes,
+  //     selectedTrainPhonemes,
+  //     dropdownPhoneme,
+  //     fiRows,
+  //     raRows,
+  //     identificationActive,
+  //
+  //     sendDropdownPhoneme,
+  //     sendForcedIdentification,
+  //     repeatPreviousPhoneme,
+  //     selectAllPhonemes,
+  //     deselectAllPhonemes
+  //   }
+  // },
   // created() {
   //   document.title = "HART Prototype - Phonemes"
   // }
