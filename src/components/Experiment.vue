697977427cfbe889c077cbb1933ffeacb7e51ca5
Pleket <50240262+Pleket@users.noreply.github.com>
2022-01-27 14:31:09 +0100

    Modal test

--- src/components/Experiment.vue
+++ src/components/Experiment.vue
@@ -2,6 +2,12 @@
   <div id="state">
       <Button @click="initialize()" class="p-shadow-2" style="padding: 0.9rem; margin-right: 10px; position: absolute;
         top: 45%; left: 45%;">Start the survey</Button>
+        <div id="myModal" class="modal" style="display: none">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+            </div>
+        </div>
   </div>
 </template>
 
@@ -12,6 +18,11 @@ export default defineComponent({
     name: 'Words',
     setup : async () => {
         function initialize() {
+            const modal = document.getElementById("myModal");
+            if (modal !== null) {
+                modal.style.display = "block";
+            }
+
             //const screen = document.getElementById("state");
 
             // ...
@@ -22,6 +33,7 @@ export default defineComponent({
 
         return {
             initialize
+
         }
     }
 });
