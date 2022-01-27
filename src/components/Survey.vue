bcfa4973955288ccf162da0d84c5a92d4dc8df01
Pleket <50240262+Pleket@users.noreply.github.com>
2022-01-27 16:05:53 +0100

    Implemented basic Survey.vue

---
+++ src/components/Survey.vue
@@ -0,0 +1,40 @@
+<template class="temp">
+  <div id="state">
+      <SelectButton v-model="selectedScale" :options="scale" optionLabel="brand">
+            <div class="scale-option">
+                <img :alt="slotProps.option.brand" :src="'demo/images/car/' + slotProps.option.brand + '.png'" />
+                <div>{{slotProps.option.brand}}</div>
+            </div>
+        </SelectButton>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import Survey from "@/components/Survey.vue";
+
+export default defineComponent({
+    name: 'Words',
+    props: ["user"],
+    components: {},
+    setup(props) {
+        const selectedScale = ref();
+        const scale = [
+            "Not different", 
+            "Somewhat different",
+            "Moderately different",
+            "Very different"
+        ]
+
+        return {
+            selectedScale,
+            scale,
+
+        }
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
