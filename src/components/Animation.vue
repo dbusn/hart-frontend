172df751c7a22f3e5c2747805cc462ddca3578b6
Pleket <50240262+Pleket@users.noreply.github.com>
2022-05-12 15:48:40 +0200

    TS changes, base design setup

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -1,18 +1,152 @@
-<script setup>
+<script setup lang="ts">
 import { onMounted } from 'vue';
 import { animate, spring, timeline } from 'motion';
 
 export default {
+    name: 'Animation',
     props: ['input'], 
-    mounted() {
-        const motor1 = animate(".1.vibrate", { scale: 1.1 },
-        {
-            duration: 1,
-            easing: spring(),
-            repeat: Infinity,
-            direction: "alternate",
-        });
+    onMounted(props : any) {
+        function createConst(i : number, duration : number) {
+            const motor = [
+                "." + i + ".lightup",
+                { color: "purple" },
+                {
+                    duration: duration,
+                    easing: spring(),
+                    direction: "alternate"
+                }
+            ];
+            return motor;
+        }
+
+        const patterns = props['input'].pattern;
+        
     }
 }
 
 </script>
+
+<template>
+    <div class="motor1"/>
+    <div class="motor2"/>
+    <div class="motor3"/>
+    <div class="motor4"/>
+    <div class="motor5"/>
+    <div class="motor6"/>
+    <div class="motor7"/>
+    <div class="motor8"/>
+    <div class="motor9"/>
+    <div class="motor10"/>
+    <div class="motor11"/>
+    <div class="motor12"/>
+    <div class="motor13"/>
+    <div class="motor14"/>
+    <div class="motor15"/>
+    <div class="motor16"/>
+    <div class="motor17"/>
+    <div class="motor18"/>
+    <div class="motor19"/>
+    <div class="motor20"/>
+    <div class="motor21"/>
+    <div class="motor22"/>
+    <div class="motor23"/>
+    <div class="motor24"/>
+</template>
+
+<style scoped>
+.motor1 {
+    
+}
+
+.motor2 {
+    
+}
+
+.motor3 {
+    
+}
+
+.motor4 {
+    
+}
+
+.motor5 {
+    
+}
+
+.motor6 {
+    
+}
+
+.motor7 {
+    
+}
+
+.motor8 {
+    
+}
+
+.motor9 {
+    
+}
+
+.motor10 {
+    
+}
+
+.motor11 {
+    
+}
+
+.motor12 {
+    
+}
+
+.motor13 {
+    
+}
+
+.motor14 {
+    
+}
+
+.motor15 {
+    
+}
+
+.motor16 {
+    
+}
+
+.motor17 {
+    
+}
+
+.motor18 {
+    
+}
+
+.motor19 {
+    
+}
+
+.motor20 {
+    
+}
+
+.motor21 {
+    
+}
+
+.motor22 {
+    
+}
+
+.motor23 {
+    
+}
+
+.motor24 {
+    
+}
+</style>
