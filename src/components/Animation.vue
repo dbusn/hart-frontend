90ca3aaffd87c15eb0bb81199039edeb1cd72b3e
Pleket <50240262+Pleket@users.noreply.github.com>
2022-05-12 15:40:17 +0200

    Base idea animation
    Using Motion One, we can now create a timeline with animations defined as a const variable returned by the createConst(i, duration) function

---
+++ src/components/Animation.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue';
+import { animate, spring, timeline } from 'motion';
+
+export default {
+    props: ['input'], 
+    mounted() {
+        const motor1 = animate(".1.vibrate", { scale: 1.1 },
+        {
+            duration: 1,
+            easing: spring(),
+            repeat: Infinity,
+            direction: "alternate",
+        });
+    }
+}
+
+</script>
